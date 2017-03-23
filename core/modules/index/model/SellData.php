07fe4bfe43ab1d74142c8f80bcc5fe7c57b0434f
djlaudio <luis.m24a@gmail.com>
2017-03-23 14:20:04 -0600

    Corrección de DB que no permitía nuevo num de factura
    Ae adjunta arriba

--- core/modules/index/model/SellData.php
+++ core/modules/index/model/SellData.php
@@ -146,7 +146,7 @@ echo $max_id;
 
   public function add_with_client(){
 
- $link=mysqli_connect("localhost","wtjjventas2018","wtjjventas2018","wtjjventas2016");
+ $link=mysqli_connect("localhost","fastness_ventas","ventas2016","fastness_wtjjventas2016");
 
 // Check connection
 if (mysqli_connect_errno()) {
